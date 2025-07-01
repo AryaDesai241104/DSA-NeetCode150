4ddd670e4827396d26619f08f03709241779173d
AryaDesai24112004 <aryadesai206@gmail.com>
2025-07-01 22:26:01 +0530

    Day 1 question added

--- Arrays and Hashing/contains_duplicate.py
+++ Arrays and Hashing/contains_duplicate.py
@@ -2,8 +2,27 @@ class Solution:
     def hasDuplicate(self, nums: list[int]) -> bool:
         hashset = set()
 
-        for n in nums :
-            if n in hashset :
+        for n in nums:
+            if n in hashset:
                 return True
             hashset.add(n)
         return False
+
+def main():
+    # Taking input from user as a string and converting it to a list of integers
+    input_str = input("Enter numbers separated by spaces: ")
+    nums = list(map(int, input_str.split()))
+
+    # Create object of Solution and call hasDuplicate
+    solution = Solution()
+    result = solution.hasDuplicate(nums)
+
+    # Print the result
+    if result:
+        print("Duplicates found!")
+    else:
+        print("No duplicates found.")
+
+if __name__ == "__main__":
+    main()
+
